cbc608bde86ecbb5c888ddb2403c950e7920e5ea
Tyson Liddell <tysonliddell@hotmail.com>
2024-07-12 21:41:35 +0100

    Draw multiple players.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,24 +12,52 @@ export class Game extends Scene
         this.load.setPath('assets');
         
         this.load.image('background', 'bg.png');
-        // this.load.image('logo', 'logo.png');
         this.load.image('tank', 'tank.png');
     }
 
     create ()
     {
         this.add.image(256, 256, 'background');
-        const tank_image = this.add.image(0, 0, 'tank').setOrigin(0,0);
-        const ws = new WebSocket("ws://192.168.1.104:8765");
+        const ws = new WebSocket("ws://127.0.0.1:8765");
+        const tank_colours = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00]
+        const tank_images = {}
         ws.onmessage = (ev) => {
-            console.log(ev.data)
-            if (ev.data.startsWith("Ack")) {
+            const data = JSON.parse(ev.data)
+            console.log(data)
+            if (data.type === "debug") {
                 return;
             }
 
-            const game_state = JSON.parse(ev.data);
-            tank_image.setX(game_state[0].px)
-            tank_image.setY(game_state[0].py)
+            switch (data.type) {
+                case "debug":
+                    return;
+                    break;
+                case "init":
+                    // const player_num = data.player_num;
+                    break;
+                case "frame":
+                    const game_state = data.state;
+                    for (const [index, player_data] of game_state.entries()) {
+                        const tank_on_screen = !!tank_images[index]
+                        if (player_data) {
+                            if (!tank_on_screen) {
+                                const tank_colour = tank_colours[index]
+                                tank_images[index] = this.add.image(0, 0, 'tank').setOrigin(0,0).setTint(tank_colour)
+                            }
+                            const tank_image = tank_images[index]
+                            tank_image.setX(game_state[index].px)
+                            tank_image.setY(game_state[index].py)
+                        } else if (tank_on_screen) {
+                            tank_images[index].destroy()
+                            delete tank_images[index]
+                        }
+                    }
+                    break;
+                default:
+                    console.log("Unknown message type: ", data.type);
+                    break;
+            }
+
         };
         this.input.keyboard.on('keydown', (ev) => {
             switch(ev.code) {
@@ -47,12 +75,5 @@ export class Game extends Scene
                     break;
             }
         });
-        // this.add.image(512, 350, 'logo').setDepth(100);
-        // this.add.text(512, 490, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-        //     fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-        //     stroke: '#000000', strokeThickness: 8,
-        //     align: 'center'
-        // }).setOrigin(0.5).setDepth(100);
-        
     }
 }
