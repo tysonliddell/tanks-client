ab13c16a07c0676358353563ff0667a43a34f7d9
Tyson Liddell <tysonliddell@hotmail.com>
2024-07-13 14:51:46 +0100

    Use Lachlan's tank image and fix rotation.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,12 +12,12 @@ export class Game extends Scene
         this.load.setPath('assets');
         
         this.load.image('background', 'bg.png');
-        this.load.image('tank', 'tank.png');
+        this.load.image('tank', 'new-tank.png');
     }
 
     create ({ server_address })
     {
-        this.add.image(256, 256, 'background');
+        this.add.image(0,0, 'background').setOrigin(0,0).setCrop(0,0,512,512);
         const ws = new WebSocket(`ws://${server_address}:8765`);
         const tank_colours = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00]
         const tank_images = {}
@@ -42,11 +42,27 @@ export class Game extends Scene
                         if (player_data) {
                             if (!tank_on_screen) {
                                 const tank_colour = tank_colours[index]
-                                tank_images[index] = this.add.image(0, 0, 'tank').setOrigin(0,0).setTint(tank_colour)
+                                tank_images[index] = this.add.image(0, 0, 'tank').setTint(tank_colour)
                             }
                             const tank_image = tank_images[index]
                             tank_image.setX(game_state[index].px)
                             tank_image.setY(game_state[index].py)
+                            switch (game_state[index].d) {
+                                case "U":
+                                    tank_image.angle = 180;
+                                    break;
+                                case "D":
+                                    tank_image.angle = 0;
+                                    break;
+                                case "L":
+                                    tank_image.angle = 90;
+                                    break;
+                                case "R":
+                                    tank_image.angle = 270;
+                                    break;
+                                default:
+                                    break;
+                            }
                         } else if (tank_on_screen) {
                             tank_images[index].destroy()
                             delete tank_images[index]
