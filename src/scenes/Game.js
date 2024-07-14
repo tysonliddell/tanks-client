29ba576b0cb3a6405c801bae0266e9b73206aa8f
Tyson Liddell <tysonliddell@hotmail.com>
2024-07-14 16:57:36 +0100

    Add respawn counter.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,7 +10,7 @@ export class Game extends Scene
     preload ()
     {
         this.load.setPath('assets');
-        
+
         this.load.image('background', 'arena-bg.png');
         this.load.image('tank', 'new-tank.png');
 
@@ -21,9 +21,15 @@ export class Game extends Scene
     {
         this.add.image(0,0, 'background').setOrigin(0,0).setCrop(0,0,512,512);
         const ws = new WebSocket(`ws://${server_address}:8765`);
-        const tank_colours = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00]
-        const tank_images = {}
-        const bullet_images = {}
+        const tank_colours = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00];
+        const tank_images = {};
+        const bullet_images = {};
+        let player_num;
+
+        const death_text = this.add.text(50, 200, 'DEAD!', { color: 'red', fontSize: '40px'});
+        const spawn_eta_text = this.add.text(50, 240, 'Respawn in TEST seconds', { color: 'red', fontSize: '20px'});
+        death_text.visible = false;
+        spawn_eta_text.visible = false;
 
         const dir_to_image_angle = (dir) => {
             switch (dir) {
@@ -38,13 +44,13 @@ export class Game extends Scene
                 default:
                     return null;
             }
-        }
+        };
 
         const move_tank = (tank_image, tank_state) => {
             tank_image.setX(tank_state.px)
             tank_image.setY(tank_state.py)
             tank_image.angle = dir_to_image_angle(tank_state.d);
-        }
+        };
 
         const move_bullet = (bullet_image, tank_state) => {
             bullet_image.setX(tank_state.bx)
@@ -52,23 +58,60 @@ export class Game extends Scene
             bullet_image.angle = dir_to_image_angle(tank_state.bd);
         }
 
+        const update_death_view = (seconds_until_respawn) => {
+            death_text.visible = true;
+            spawn_eta_text.text = `Respawn in ${seconds_until_respawn.toFixed(4)} seconds`;
+            spawn_eta_text.visible = true;
+        };
+
+        const hide_death_view = () => {
+            death_text.visible = false;
+            spawn_eta_text.visible = false;
+        };
+
         ws.onmessage = (ev) => {
             const data = JSON.parse(ev.data)
             console.log(data)
-            if (data.type === "debug") {
-                return;
-            }
 
             switch (data.type) {
                 case "debug":
                     return;
-                    break;
                 case "init":
-                    // const player_num = data.player_num;
+                    player_num = data.player_num;
                     break;
                 case "frame":
                     const game_state = data.state;
-                    for (const [index, player_data] of game_state.entries()) {
+                    const missing_players = game_state.reduce((acc, player_data, idx) => {
+                        if (!player_data) {
+                            acc.push(idx);
+                        }
+                        return acc;
+                    }, []);
+                    const alive_players = game_state.filter(p => !!p && !p.x)
+                    const dead_players = game_state.filter(p => !!p && !!p.x)
+                    const dead_player_indexes = dead_players.map(p => p.id - 1);
+
+                    if (dead_player_indexes.includes(player_num-1)) {
+                        update_death_view(game_state[player_num-1].xs);
+                    } else {
+                        hide_death_view();
+                    }
+
+                    for (const index of [...missing_players, ...dead_player_indexes]) {
+                        const tank_on_screen = !!tank_images[index];
+                        const bullet_on_screen = !!bullet_images[index];
+                        if (tank_on_screen) {
+                            tank_images[index].destroy()
+                            delete tank_images[index]
+                        }
+                        if (bullet_on_screen) {
+                            bullet_images[index].destroy()
+                            delete bullet_images[index]
+                        }
+                    }
+
+                    for (const player_data of alive_players) {
+                        const index = player_data.id - 1;
                         const tank_state = game_state[index];
                         const tank_on_screen = !!tank_images[index];
                         const should_draw_bullet = !!tank_state?.bd;
